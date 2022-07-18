2ff922156cd5204b0b7c6fa570ad849bf5ce9641
estimar2 <songsm5504@gmail.com>
2022-07-18 18:04:21 +0900

    install react-router-dom, axios ... etc

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const App = () => {
+  useEffect(() => {
+    _getUser();
+  }, []);
+
+  const _getUser = async () => {
+    await axios
+      .get("/api/users")
+      .then(res => {
+        if (res.status === 200) {
+          const resultData = res.data;
+
+          console.log(resultData, "확인");
+        } else if (res.status === 400) {
+          alert("res.status === 400");
+        }
+      })
+      .catch(e => console.error(e));
+  };
+
   return <div>Hello pagination</div>;
 };
 
